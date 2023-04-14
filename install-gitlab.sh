3d1e0bf6e962298c75f25667b403104e3238e7ef
Kenson Johnson <kenson.johnson@outlook.com>
2023-04-13 23:44:35 -0500

    Disabling interactivity

--- install-gitlab.sh
+++ install-gitlab.sh
@@ -8,16 +8,20 @@ then
 else 
     echo -e "Installing GitLab"
     # Get dependencies
-    sudo apt update
-    sudo apt install -y curl openssh-server ca-certificates tzdata perl
+    sudo apt-get update
+    sudo apt-get install -y curl openssh-server ca-certificates tzdata perl
 
     # Install smtp server
-    sudo DEBIAN_FRONTEND=noninteractive apt install -y postfix
+    # debconf-set-selections <<< "postfix postfix/mailname string your.hostname.com"
+    # debconf-set-selections <<< "postfix postfix/main_mailer_type string 'No Configuration'"
+    echo "postfix postfix/main_mailer_type select 'No Configuration" | debconf-set-selections
+    echo "postfix postfix/mailname string relative.path" | debconf-set-selections
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y postfix
 
     # Download GitLab and run installer
     curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
 
     # TODO: Setup external URL
-    # sudo EXTERNAL_URL="https://gitlab.example.com" apt-get install gitlab-ee
+    sudo EXTERNAL_URL="https://gitlab.example.com" apt-get install gitlab-ee
 
 fi
